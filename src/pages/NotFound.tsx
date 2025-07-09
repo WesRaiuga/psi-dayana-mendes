9cccaf802fa8abea71cad576d433d49f3de3dc23
Wes <weslley_wenny@hotmail.com>
2025-07-09 15:31:04 -0300

    refactor NotFound component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <>
-      <h1>404 - Página não encontrada</h1>
+    <section>
+      <h2>404 - Página não encontrada</h2>
       <p>Ops! A página que você tentou acessar não existe.</p>
       <Link to="/">Voltar para a página inicial</Link>
-    </>
+    </section>
   );
 };
 
