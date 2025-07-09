0b5ca0762fe01aae84a87c788612587bed7e8610
Wes <weslley_wenny@hotmail.com>
2025-07-09 15:09:01 -0300

    remove basename from BrowserRouter for Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/psi-dayana-mendes/'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
