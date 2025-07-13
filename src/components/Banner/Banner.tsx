799f7333a99e79a473bfe8b58864316745319f9a
Wes <weslley_wenny@hotmail.com>
2025-07-13 19:40:16 -0300

    ajusts and add shadcn/sparkles

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 import style from "./Banner.module.css";
 import WhatsappButton from "../WhatsappButton/WhatsappButton";
+import { SparklesCore } from "../ui/sparkles";
 
 const Banner = () => {
   return (
-    <div id="banner" className={style.bannerContent}>
-      <h1>Dayana Mendes</h1>
-      <p>Psicóloga Clínica</p>
-      <WhatsappButton />
+    <div id="banner" className={style.bannerContainer}>
+      <SparklesCore id="tsparticlesfullpage" background="transparent" minSize={0.6} maxSize={2.4} particleDensity={100} className="w-full h-full" particleColor="#8772cf" />
+      <div className={style.bannerContent}>
+        <div>
+          <h1>Dayana Mendes</h1>
+          <p>Psicóloga Clínica</p>
+        </div>
+        <WhatsappButton />
+      </div>
       <div className={style.scrollArrow}>
         <Link to="/" state={{ scrollTo: "about-me" }}>
           <i className="fa-solid fa-chevron-down"></i>
