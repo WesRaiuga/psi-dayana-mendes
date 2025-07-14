99e4240db48436d3f52afab3f007fe330779077a
Wes <weslley_wenny@hotmail.com>
2025-07-13 22:16:59 -0300

    adjusts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import AboutMeSection from "../../views/AboutMeSection/AboutMeSection";
-import EducationSection from "../../views/EducationSection/EducationSection";
 import Divider from "../../components/Divider/Divider";
 import FaqSection from "../../views/FaqSection/FaqSection";
 import SymptomsSection from "../../views/SymptomsSection/SymptomsSection";
@@ -28,8 +27,7 @@ const Home = () => {
       <Banner />
       {/* <div className={style.gradient}></div> */}
       <AboutMeSection />
-      <Divider color="#5b99a1" />
-      <EducationSection />
+      <Divider color="#bbcacc" />
       <SymptomsSection />
       <HowItWorksSection />
       <ApproachSection />
