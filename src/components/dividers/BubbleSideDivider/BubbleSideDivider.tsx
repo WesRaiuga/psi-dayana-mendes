0c506499eeab72e5f2347d107ee4349721b0250b
Wes <weslley_wenny@hotmail.com>
2025-07-19 20:50:37 -0300

    fix bubbleSide divider

--- src/components/dividers/BubbleSideDivider/BubbleSideDivider.tsx
+++ src/components/dividers/BubbleSideDivider/BubbleSideDivider.tsx
@@ -6,7 +6,7 @@ const Divider: React.FC<DividerProps> = ({ color }) => {
   return (
     <div className="relative">
       <div className="absolute bottom-0 w-full overflow-hidden leading-none">
-        <svg viewBox="0 0 500 150" preserveAspectRatio="none" className={`w-full h-16 fill-[${color}]`}>
+        <svg viewBox="0 0 500 150" preserveAspectRatio="none" fill={color} className={`w-full h-16`}>
           <path d="M0.00,49.98 C150.00,150.00 350.00,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"></path>
         </svg>
       </div>
