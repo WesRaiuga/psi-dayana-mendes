f5a02418aeeb1c31b9dd732faaf5afb2341a810d
Wes <weslley_wenny@hotmail.com>
2025-08-02 15:21:57 -0300

    add google tag report conversion to whatsapp button

--- src/components/WhatsappButton/WhatsappButton.tsx
+++ src/components/WhatsappButton/WhatsappButton.tsx
@@ -1,11 +1,40 @@
 import React from "react";
 import style from "./WhatsappButton.module.css";
-import { Link } from "react-router-dom";
+
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
+const url = "https://wa.me/5511960765640?text=Oi%20Day,%20vim%20pelo%20seu%20site%20e%20gostaria%20de%20agendar%20uma%20sessão";
+
+const gtag_report_conversion = (url: string) => {
+  const callback = () => {
+    if (typeof url != "undefined") {
+      window.location.href = url;
+    }
+  };
+  window.gtag("event", "conversion", {
+    send_to: "AW-17381606924/d6syCNTZp_4aEIyMmeBA",
+    event_callback: callback,
+  });
+  return false;
+};
 
 const WhatsappButton: React.FC = () => (
-  <Link to="https://wa.me/5511960765640?text=Oi%20Day,%20vim%20pelo%20seu%20site%20e%20gostaria%20de%20agendar%20uma%20sessão" className={style.wppLink} target="_blank" rel="noopener noreferrer">
+  <a
+    href={url}
+    className={style.wppLink}
+    target="_blank"
+    rel="noopener noreferrer"
+    onClick={(e) => {
+      e.preventDefault();
+      gtag_report_conversion(url);
+    }}
+  >
     Agende sua sessão
-  </Link>
+  </a>
 );
 
 export default WhatsappButton;
